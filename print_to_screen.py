440941d222ebda4c4ad97f13911733fe0d64f774
Jakob Kjeldsteen <115626970+jkjeldsteen@users.noreply.github.com>
2024-02-28 12:21:16 +0100

    Update print_to_screen.py

--- print_to_screen.py
+++ print_to_screen.py
@@ -5,6 +5,7 @@ import time					  # Time lib
 # init OLED
 # using default address 0x3C
 #i2c = I2C(sda=Pin(4), scl=Pin(5))
+def printToScreen():
 i2c = I2C(1, scl = Pin(22), sda = Pin(21), freq = 400000)
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 display.text('Firmware updated',0,0,1)
